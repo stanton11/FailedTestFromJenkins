17a3291dd036554b223975621b74b1117fc8880a
Mike <mike.stanton@couchbase.com>
2024-01-30 15:22:52 +0000

    fix

--- getParams.py
+++ getParams.py
@@ -49,12 +49,12 @@ def format_output(params):
     for key in params:
         if key == "lpv" or key == "enable_istio" or key == "platformcert":
             output_string.append(
-                f"[booleanParam(name: '{key}', value: '{params[key]}'),"
+                f"booleanParam(name: '{key}', value: '{params[key]}'),"
             )
         elif key == "validation" or key == "sanity" or key == "p0" or key == "p1":
-            output_string.append(f"[booleanParam(name: '{key}', value: 'false'),")
+            output_string.append(f"booleanParam(name: '{key}', value: 'false'),")
         else:
-            output_string.append(f"[string(name: '{key}', value: '{params[key]}'),")
+            output_string.append(f"string(name: '{key}', value: '{params[key]}'),")
 
     # Join the list of strings into a single string
     result_string = "".join(output_string)
